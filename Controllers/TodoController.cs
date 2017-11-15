c054e5747b2710b7f584f0b9666e224625ba048a
mgudgila <mahesh@thinkbridge.in>
2017-11-15 09:26:46 +0530

    Created TodoController wth dbContext injection

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+using System.Linq;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+    }
+}
